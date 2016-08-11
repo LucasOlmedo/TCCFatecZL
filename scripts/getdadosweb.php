320c7e05fac2224e2e3f2da09029624f4d766c73
jcdourado <jcd17071997@hotmail.com>
2016-08-11 09:07:07 -0300

    Termino dos scripts de captura dos dados

---
+++ scripts/getdadosweb.php
@@ -0,0 +1,15 @@
+<?php 
+	// Script que pega as turmas e os professores
+
+	$con = new PDO('mysql:host=localhost;dbname=tcc_fateczl','root','123456');
+
+	foreach($con->query('SELECT nome FROM PROFESSOR') as $row) {
+ 		echo json_encode($row);
+	}
+
+	foreach($con->query('SELECT DISTINCT CURSO.nome_curso, AULASEMESTRAL.turno, AULASEMESTRAL.id_periodo FROM AULASEMESTRAL INNER JOIN CURSO ON AULASEMESTRAL.ID_CURSO = CURSO.ID_CURSO ') as $row){
+		echo json_encode($row);
+	}
+
+
+?>
---
+++ scripts/getdiasemana.php
@@ -0,0 +1,48 @@
+<?php
+
+	// Script que pega os dias semana de acordo com os dados passados
+	// Dados: Periodo - Curso - Turno ou Professor
+
+	$con = new PDO('mysql:host=localhost;dbname=tcc_fateczl','root','123456');
+
+	/* Teste para ver se a conexao ta funcionando
+
+		foreach($con->query('SELECT * FROM AULASEMESTRAL') as $row){
+			echo json_encode($row) . '</br>';		
+		}
+	*/	
+
+	if(isset($_GET['professor'])){
+
+		$psmt = $con->prepare('SELECT professor.nome, disciplina.abreviacao, DIASEMANA.horario_inicio, DIASEMANA.horario_fim  FROM DIASEMANA INNER JOIN Professor ON DIASEMANA.id_professor = Professor.id_Professor INNER JOIN Disciplina ON DIASEMANA.id_Disciplina = Disciplina.id_Disciplina WHERE DIASEMANA.id_professor = ? ');
+		$psmt->bindParam(1 , $_GET['professor']);
+		if($psmt->execute()){
+			while($rs = $psmt->fetch()){
+				echo json_encode($rs);
+			}
+		}
+		$psmt = null;
+	}
+	else{
+
+		$psmt = $con->prepare('SELECT professor.nome, disciplina.abreviacao, DIASEMANA.horario_inicio, DIASEMANA.horario_fim FROM DIASEMANA INNER JOIN Professor ON DIASEMANA.id_professor = Professor.id_Professor INNER JOIN Disciplina ON DIASEMANA.id_Disciplina = Disciplina.id_Disciplina WHERE DIASEMANA.id_periodo = ? AND DIASEMANA.id_curso = ? AND DIASEMANA.turno = ?');
+		$psmt->bindParam(1, $_GET['periodo']);
+		$psmt->bindParam(2, $_GET['curso']);
+
+		$psmt->bindParam(3, $_GET['turno']);
+
+		if($psmt->execute()){
+			while($rs = $psmt->fetch()){
+				echo json_encode($rs);
+			}
+		}
+
+		$psmt = null;
+	}
+	
+	$con = null;
+
+
+
+
+?>
