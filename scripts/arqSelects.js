9578f296db6e524ed79b3c68e44f4b71a3c8a76f
jcdourado <jcd17071997@hotmail.com>
2016-08-12 11:30:30 -0300

    Requisicoes ajax funcionando

---
+++ scripts/arqSelects.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function(){
+	var selectCurso = document.getElementById('select-curso');
+ 	var selectProfessor = document.getElementById('select-professor');
+
+ 	selectCurso.addEventListener('change',function(){
+ 		if(selectProfessor.selectedIndex != 0){
+ 			selectProfessor.selectedIndex = 0;
+ 		}
+ 		replaceTextGrade(selectCurso.options[selectCurso.selectedIndex].text);
+		openAjaxTurma(selectCurso.value);
+ 	});	
+
+ 	selectProfessor.addEventListener('change',function(){
+ 		if(selectCurso.selectedIndex != 0){
+ 			selectCurso.selectedIndex = 0;
+		}
+		replaceTextGrade('Professor(a) ' + selectProfessor.options[selectProfessor.selectedIndex].text);
+		openAjaxProfessor(selectProfessor.value);
+	});
+
+});
+
+function replaceTextGrade(text){
+	document.getElementById('text-grade').innerHTML = text;
+}
+
+function openAjaxProfessor(idProfessor){
+	var req;
+
+	if(window.XMLHttpRequest){
+		req = new XMLHttpRequest();
+	} else{
+		req = new new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	req.onreadystatechange = function(){
+		if(req.readyState == 4){
+			return exibe(req.responseText);
+		}
+	}
+	req.open('GET','getdiasemana.php?professor='+idProfessor,true);
+	req.send();
+
+}
+
+function openAjaxTurma(idTurma){
+	var arr = idTurma.split('-');
+	var curso = arr[0];
+	var periodo = arr[1];
+	var turno = arr[2];
+
+	var req;
+
+	if(window.XMLHttpRequest){
+		req = new XMLHttpRequest();
+	} else{
+		req = new new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	req.onreadystatechange = function(){
+		if(req.readyState == 4){
+			return exibe(req.responseText);
+		}
+	}
+	req.open('GET','getdiasemana.php?periodo='+periodo+'&curso='+curso+'&turno='+turno,true);
+	req.send();
+}
+
+function exibe(text){
+	console.log(text);
+}
--- scripts/gradehoraria.php
+++ scripts/gradehoraria.php
@@ -6,53 +6,147 @@
 <html>
 <head>
 	<title>Grade Horária - FATEC Zona Leste</title>
-	<script>
-		document.addEventListener("DOMContentLoaded", function(){
-			var selectCurso = document.getElementById('select-curso');
-		 	var selectProfessor = document.getElementById('select-professor');
-
-		 	selectCurso.addEventListener('change',function(){
-		 		if(selectProfessor.selectedIndex != 0){
-		 			selectProfessor.selectedIndex = 0;
-		 		}
-		 	});	
-
-		 	selectProfessor.addEventListener('change',function(){
-		 		if(selectCurso.selectedIndex != 0){
-		 			selectCurso.selectedIndex = 0;
-		 		}
-		 	});
-	 	});
-
-
-	</script>
-
+	<script src="arqSelects.js"></script>
 </head>
 <body>
-	<h1>Grade Horária Fatec Zona Leste</h1>
-	<select id="select-curso">
-		<option value="0">Escolha uma turma</option>
-		<?php
-			foreach($con->query('SELECT DISTINCT AULASEMESTRAL.id_curso, CURSO.nome_curso, AULASEMESTRAL.turno, AULASEMESTRAL.id_periodo FROM AULASEMESTRAL INNER JOIN CURSO ON AULASEMESTRAL.ID_CURSO = CURSO.ID_CURSO ') as $row){
-				$turn = '';
-				switch($row['turno']){
-					case 0: $turn = 'Manhã'; break;
-					case 1: $turn = 'Tarde'; break;
-					case 2: $turn = 'Noite'; break;
+	<header>
+		<h1>Grade Horária Fatec Zona Leste</h1>
+	</header>
+	<nav>
+		<select id="select-curso">
+			<option value="0">Escolha uma turma</option>
+			<?php
+				foreach($con->query('SELECT DISTINCT AULASEMESTRAL.id_curso, CURSO.nome_curso, AULASEMESTRAL.turno, AULASEMESTRAL.id_periodo FROM AULASEMESTRAL INNER JOIN CURSO ON AULASEMESTRAL.ID_CURSO = CURSO.ID_CURSO ') as $row){
+					$turn = '';
+					switch($row['turno']){
+						case 0: $turn = 'Manhã'; break;
+						case 1: $turn = 'Tarde'; break;
+						case 2: $turn = 'Noite'; break;
+					}
+					echo '<option value=' . $row['id_curso'] . '-' . $row['id_periodo'] . '-' . $row['turno'] .'>' . $row['id_periodo'] . 'º ' . $row['nome_curso'] . ' ' . $turn . '</option>';	
 				}
-				echo '<option value=' . $row['id_curso'] . '-' . $row['id_periodo'] . '-' . $row['turno'] .'>' . $row['turno'] . 'º ' . $row['nome_curso'] . ' ' . $turn . '</option>';	
-			}
-		 ?>
-	</select>
-	<select id="select-professor">
-		<option value="0">Escolha um professor</option>
-		<?php 
-			foreach($con->query('SELECT nome, id_professor FROM PROFESSOR') as $row) {
- 				echo '<option value="' . $row['id_professor'] .'">' . $row['nome'] . '</option>';
-			}
+			 ?>
+		</select>
+		<select id="select-professor">
+			<option value="0">Escolha um professor</option>
+			<?php 
+				foreach($con->query('SELECT nome, id_professor FROM PROFESSOR') as $row) {
+	 				echo '<option value="' . $row['id_professor'] .'">' . $row['nome'] . '</option>';
+				}
 
-			$con = null;
-		?>
-	</select>
+				$con = null;
+			?>
+		</select>
+	</nav>
+
+	<p id='p'></p>
+	<section>
+		<table border="1">
+			<thead>
+				<h3 id=text-grade></h3>
+			</thead>
+			<tbody>
+				<tr>
+					<td></td>
+					<td>7:30 - 8:20</td>
+					<td>8:20 - 9:10</td>
+					<td>9:20 - 10:10</td>
+					<td>10:10 - 11:00</td>
+					<td>11:10 - 12:00</td>
+					<td>12:00 - 12:50</td>
+					<td>13:00 - 14:40</td>
+					<td>14:50 - 16:30</td>
+					<td>16:40 - 18:20</td>
+					<td>19:20 - 21:00</td>
+					<td>21:10 - 22:50</td>
+				</tr>	
+				<tr>
+					<td>Seg</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Ter</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Qua</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Qui</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Sex</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Sab</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	</section>
+		
 </body>
 </html>
