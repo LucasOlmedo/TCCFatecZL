bdf4c179c7bfa92234e5ed74632b2477f2b94225
jcdourado <jcd17071997@hotmail.com>
2016-08-12 10:04:42 -0300

    Commit criacao da grade horaria

--- scripts/gradehoraria.php
+++ scripts/gradehoraria.php
@@ -1,55 +1,58 @@
+<?php 
+	$con = new PDO('mysql:host=localhost;dbname=tcc_fateczl','root','123456');
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Grade Horária - FATEC Zona Leste</title>
 	<script>
 		document.addEventListener("DOMContentLoaded", function(){
-			var ajax;
+			var selectCurso = document.getElementById('select-curso');
+		 	var selectProfessor = document.getElementById('select-professor');
 
-			if(window.XMLHttpRequest){
-				ajax = new XMLHttpRequest();
-			}
-			else{
-				ajax = new ActiveXObject("Microsoft.XMLHTTP");
-			}
+		 	selectCurso.addEventListener('change',function(){
+		 		if(selectProfessor.selectedIndex != 0){
+		 			selectProfessor.selectedIndex = 0;
+		 		}
+		 	});	
 
-			ajax.onreadystatechange = function(){
-	 			if(ajax.readyState == 4 ){
-					preencher(ajax.responseText);
-	 			}
-	 		}
-	 		ajax.open("GET","getdadosweb.php",true);
-	 		ajax.send();
-
-
-	 		function preencher(dados){
-	 			dados = dados.replace(',]',']');
-
-	 			//var textCurso = "<option>";
-	 			//var textProfessor = "<option>";
-	 			//var myArr = JSON.parse(dados);
-
-	 			//for(var i = 0; i < myArr.length; i++){
-	 				//textCurso += 
-	 				//textProfessor += myArr[i].nome;
-	 			//}
-	 			console.log(JSON.parse(dados));
-
-	 		}
-		});
+		 	selectProfessor.addEventListener('change',function(){
+		 		if(selectCurso.selectedIndex != 0){
+		 			selectCurso.selectedIndex = 0;
+		 		}
+		 	});
+	 	});
 
 
 	</script>
 
-
 </head>
 <body>
 	<h1>Grade Horária Fatec Zona Leste</h1>
-	<select id="select-curso" disabled>
+	<select id="select-curso">
 		<option value="0">Escolha uma turma</option>
+		<?php
+			foreach($con->query('SELECT DISTINCT AULASEMESTRAL.id_curso, CURSO.nome_curso, AULASEMESTRAL.turno, AULASEMESTRAL.id_periodo FROM AULASEMESTRAL INNER JOIN CURSO ON AULASEMESTRAL.ID_CURSO = CURSO.ID_CURSO ') as $row){
+				$turn = '';
+				switch($row['turno']){
+					case 0: $turn = 'Manhã'; break;
+					case 1: $turn = 'Tarde'; break;
+					case 2: $turn = 'Noite'; break;
+				}
+				echo '<option value=' . $row['id_curso'] . '-' . $row['id_periodo'] . '-' . $row['turno'] .'>' . $row['turno'] . 'º ' . $row['nome_curso'] . ' ' . $turn . '</option>';	
+			}
+		 ?>
 	</select>
-	<select id="select-professor" disabled>
+	<select id="select-professor">
 		<option value="0">Escolha um professor</option>
+		<?php 
+			foreach($con->query('SELECT nome, id_professor FROM PROFESSOR') as $row) {
+ 				echo '<option value="' . $row['id_professor'] .'">' . $row['nome'] . '</option>';
+			}
+
+			$con = null;
+		?>
 	</select>
 </body>
 </html>
