41a006b36f77e8c6ff4e588ef244650b7329db67
jcdourado <jcd17071997@hotmail.com>
2016-09-13 10:12:31 -0300

    Commit do relatorio com as funcoes completas

--- Impressao/Mensal/mensal.js
+++ Impressao/Mensal/mensal.js
@@ -9,36 +9,37 @@ function pegarValues(){
 	var ano = mesAno.value.split('-')[0];
 	var mes = mesAno.value.split('-')[1];
 
-	var data = new Date(ano,mes-1);
+	var data = new Date(ano,mes,0);
 
-	var lastDay = ultimoDiaDoMes(data);
-	preencherTable(lastDay);	
+	preencherTable(data);	
 }
 
-function ultimoDiaDoMes(objDate){
- 	return (new Date(objDate.getFullYear(), objDate.getMonth() + 1, 0) ).getDate();
-}
-
-function preencherTable(lastDay){
+function preencherTable(day){
 	var table = document.getElementById('table-frequencia');
 
 	while (table.firstChild) {
     	table.removeChild(table.firstChild);
 	}
 
+	var lastDay = day.getDate();
+
 	for(var i = 1; i <= lastDay ; i++){
 		var row = table.insertRow(-1);
 
+		day.setDate(i);
 		for(k = 0; k < 20; k++){
 			var x = row.insertCell(-1);
 
 			if(i < 10 && k == 0){
 				x.innerHTML = "0" + i;
 			}
-			else if(k==0){
+			else if(k == 0){
 				x.innerHTML = i;
 			}
 
+			if(day.getDay() == 0 && (k == 1 || k == 8 || k == 15)){
+				x.innerHTML = "Domingo";
+			}
 		}
 
 	}
