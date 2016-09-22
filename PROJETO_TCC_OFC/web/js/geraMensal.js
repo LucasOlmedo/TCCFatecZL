01b8bb6d3d5eb3ec4adb56718cfda0b0928edd4b
jcdourado <jcd17071997@hotmail.com>
2016-09-22 13:32:28 -0300

    Commit fim do dia, btn dos profs atualizados

--- PROJETO_TCC_OFC/web/js/geraMensal.js
+++ PROJETO_TCC_OFC/web/js/geraMensal.js
@@ -1,22 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var table = document.getElementsByTagName('table')[0];
 
-	for(var j = 2; j < table.rows.length ; j++){
+	console.log(table.rows[2].cells.length);
+	if(!(table.rows[2].cells.length == 1)) {
+		for (var j = 2; j < table.rows.length; j++) {
 
-		var link = document.createElement('a');
-		var helper = document.createElement('i');
-		var id_professor = table.rows[j].cells[0].innerHTML;
+			var link = document.createElement('a');
+			var helper = document.createElement('i');
+			var id_professor = table.rows[j].cells[0].innerHTML;
 
-		helper.setAttribute('class','glyphicon glyphicon-calendar');
-		helper.style.margin = 'auto 7px';
-		link.setAttribute('title','Relatório Mensal');
-		link.setAttribute('aria-label','Mensal');
-		link.appendChild(helper);
-		link.setAttribute('href','mensal/mensal.php?id='+id_professor);
-		link.setAttribute('target','_blank');
+			helper.setAttribute('class', 'glyphicon glyphicon-calendar');
+			helper.style.margin = 'auto 7px';
+			link.setAttribute('title', 'Relatório Mensal');
+			link.setAttribute('aria-label', 'Mensal');
+			link.appendChild(helper);
+			link.setAttribute('href', 'mensal/mensal.php?id=' + id_professor);
+			link.setAttribute('target', '_blank');
 
-		table.rows[j].cells.item(table.rows[j].cells.length -1).appendChild(link);
+			table.rows[j].cells.item(table.rows[j].cells.length - 1).appendChild(link);
 
+		}
 	}
 
 
